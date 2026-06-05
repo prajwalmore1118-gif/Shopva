2ef2953414367846b3f77bf8accd87416796bc85
prajwalmore1118-gif <prajwalmore1118@gmail.com>
2026-06-05 20:20:27 +0530

    Added AssetDemo component

---
+++ src/assets/AssetDemo.jsx
@@ -0,0 +1,10 @@
+const AssetDemo = () => {
+  return (
+    <div>
+      <h2>Assets Demo Component</h2>
+      <p>This is a sample component inside assets.</p>
+    </div>
+  );
+};
+
+export default AssetDemo;
---
+++ src/assets/index.jsx
@@ -0,0 +1,9 @@
+import logo from './images/logo.png';
+import heroBanner from './images/hero-banner.jpg';
+import cartIcon from './icons/cart.svg';
+
+export {
+  logo,
+  heroBanner,
+  cartIcon,
+};
